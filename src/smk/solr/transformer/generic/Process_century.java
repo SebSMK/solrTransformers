ed4bb1123efd05ac27c95db2ef3a044d30bc2a8e
sbr_hus <sbr@hus.dk>
2014-11-18 17:04:01 +0100

    [FEATURE] process_century v0.1

---
+++ src/smk/solr/transformer/generic/Process_century.java
@@ -0,0 +1,45 @@
+package smk.solr.transformer.generic;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.commons.lang.StringUtils;
+
+import smk.solr.transformer.generic.Util;
+
+public class Process_century {
+	
+	/**
+     * Process fields that must be capitalized
+     **/            
+     public Object transformRow(Map<String, Object> row) {
+    	 Object prod_earlier_date = row.get("object_production_date_earliest");
+    	 Date date = new Date((Long) prod_earlier_date); 
+       
+       Calendar calendar = Calendar.getInstance();
+       calendar.setTime(date);
+
+       int century = (calendar.get(Calendar.YEAR) / 100) +1;
+       
+       row.put("object_production_century_earliest", Integer.toString(century));
+ 
+//    	 row.put("object_production_century_earliest", prod_earlier_date);
+    	 
+//         if(Util.isValidDataText(prod_earlier_date)){
+//           prod_earlier_date = prod_earlier_date.replaceAll("CET", "EDT");
+//           prod_earlier_date = prod_earlier_date.replaceAll("CEST", "EDT");
+//           Date date = new Date(prod_earlier_date); 
+//           
+//           Calendar calendar = Calendar.getInstance();
+//           calendar.setTime(date);
+//
+//           int century = (calendar.get(Calendar.YEAR) / 100) +1;
+//           
+//           row.put("object_production_century_earliest", Integer.toString(century));
+//         }        
+         
+         return row;
+     }	
+
+}
