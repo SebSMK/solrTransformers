d7ff2c9e3b9f5f493a2ec17b31442b85dfc512a8
Chris Pott <christopher.pott@gmail.com>
2015-01-30 12:40:46 +0100

    New transform to clean up ENB status field

---
+++ solr-transformer/src/main/java/org/smk/solr/transformer/enb/Process_enb_clean.java
@@ -0,0 +1,29 @@
+package org.smk.solr.transformer.enb;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.smk.solr.transformer.generic.Util;
+
+public class Process_enb_clean {
+
+	protected final static Logger log = Logger .getLogger(Process_enb_clean.class);
+	
+	/**
+	 * 
+	 **/
+	public Object transformRow(Map<String, Object> row) {
+		if(log.isDebugEnabled()){
+			log.debug(String.format("--------\r\nstart Process_enb_clean - id:%s", (String) row.get("id")));
+		}
+		String sikkerhedstatus = (String)row.get("sikkerhedstatus");
+		
+		if (sikkerhedstatus != null && !sikkerhedstatus.equals("ENB-værk") && !sikkerhedstatus.equals("NB-værk")){
+			row.remove("sikkerhedstatus");
+		}
+		if(log.isDebugEnabled()){
+			log.debug(String.format("finish Process_enb_clean - id:%s\r\n--------------", (String) row.get("id")));
+		}
+		return row;
+	}
+}
