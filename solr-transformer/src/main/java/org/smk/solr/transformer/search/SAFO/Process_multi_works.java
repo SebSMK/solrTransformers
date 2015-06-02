b251c48616ccd2e2bb3ab0b1c4258655d37d1524
seb <seb@csdev-seb.SMK.DK>
2015-06-02 13:38:11 +0200

    [DRIFT] search_SAFO functions extend generic functions

---
+++ solr-transformer/src/main/java/org/smk/solr/transformer/search/SAFO/Process_multi_works.java
@@ -0,0 +1,3 @@
+package org.smk.solr.transformer.search.SAFO;
+
+public class Process_multi_works extends org.smk.solr.transformer.generic.Process_multi_works{} 
---
+++ solr-transformer/src/main/java/org/smk/solr/transformer/search/SAFO/Process_producents.java
@@ -0,0 +1,3 @@
+package org.smk.solr.transformer.search.SAFO;
+
+public class Process_producents extends org.smk.solr.transformer.generic.Process_producents{}
